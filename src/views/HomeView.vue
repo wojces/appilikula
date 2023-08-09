f9c61f70ef2a19e1374fb23073db10dd5c168d17
Wojciech <wojtekc321@gmail.com>
2023-08-09 23:11:20 +0200

    change prettiers configuration for empty space

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-  <!--
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">
+        Email address
+      </label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">
+        Example textarea
+      </label>
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        rows="3"
+      ></textarea>
+    </div>
+    <!--
     2 inputy:
       - Gracz A
       - Wynik gracza A
       - Gracz B
       - Wynik bracza B
-      ZAPISZ
-  -->
-  </div>
+      ZAPISZ  
+  --></div>
 </template>
 
 <script lang="ts">
